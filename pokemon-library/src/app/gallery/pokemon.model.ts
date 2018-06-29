8d069742e3d85fa0d02807f60fa0dc3011348860
Marcin <michniak@yandex.com>
2018-06-29 12:11:36 +0200

    Change navbar-collapse to navbar-default for small screen support. Create model for pokemons.

---
+++ pokemon-library/src/app/gallery/pokemon.model.ts
@@ -0,0 +1,16 @@
+export class Recipe{
+    public description: string;
+    public imagePath: string;
+
+    private name: string;
+
+    constructor(name: string, desc: string, imgPath: string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imgPath;
+    }
+
+    public getName(){
+        return this.name;
+    }
+}
